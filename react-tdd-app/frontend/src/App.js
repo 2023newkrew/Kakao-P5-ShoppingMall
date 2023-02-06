272823cd2c78548be543c0f03b459d40920e01ce
gray-sin <gray.sin@kakaocorp.com>
2023-02-06 17:26:23 +0900

    test impl: App handles server error

--- react-tdd-app/frontend/src/App.js
+++ react-tdd-app/frontend/src/App.js
@@ -3,12 +3,23 @@ import "./App.css";
 
 function App({ url }) {
   const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(`${url}/products`)
       .then((response) => response.json())
-      .then(setProducts);
+      .then(setProducts)
+      .catch(setError);
   }, [url]);
+
+  if (error) {
+    return (
+      <div className="App">
+        <h2>문제가 발생했습니다.</h2>
+        <p>{error.toString()}</p>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
