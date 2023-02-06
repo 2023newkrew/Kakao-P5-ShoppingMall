674441781144124f40fc0d1c5ffe01d151aea100
gray-sin <gray.sin@kakaocorp.com>
2023-02-06 16:48:06 +0900

    App 컴포넌트에 대한 테스트 생성

--- react-tdd-app/frontend/src/App.test.js
+++ react-tdd-app/frontend/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders heading", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByRole("heading");
+  expect(headingElement).toHaveTextContent("Travel Products");
+});
+
+test("renders loading", async () => {
+  render(<App />);
+  const loadingElement = await screen.findByText("loading...");
+  expect(loadingElement).toBeInTheDocument();
 });
