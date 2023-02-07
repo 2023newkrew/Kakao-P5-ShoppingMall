04aa6ef86b149e4608d5eed4e74839c5614e373a
Kim Seung Hwan <dev.appmin@gmail.com>
2023-02-07 13:44:52 +0900

    refactor: Refactor client w/ constants and .env

---
+++ react-tdd-app/frontend/src/apis/client.ts
@@ -0,0 +1,21 @@
+import { API_ROUTE, API_ROUTE_PATH } from '@/constants/api';
+
+export const getProducts = async () => {
+  const res = await fetch(`${process.env.REACT_APP_API_URL}${API_ROUTE_PATH[API_ROUTE.PRODUCTS]}`);
+  return res.json();
+};
+
+export const getOptions = async () => {
+  const res = await fetch(`${process.env.REACT_APP_API_URL}${API_ROUTE_PATH[API_ROUTE.OPTIONS]}}`);
+  return res.json();
+};
+
+export const createOrder = async () => {
+  const res = await fetch(`${process.env.REACT_APP_API_URL}${API_ROUTE_PATH[API_ROUTE.ORDERS]}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.json();
+};
