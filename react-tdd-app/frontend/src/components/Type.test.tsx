70ff7ff1b4725aea25d660ae6a8c4f6251bbec67
은승균 <dmstmdrbs98@gmail.com>
2023-02-07 18:12:58 +0900

    test: Product, Option 유저 동작에 대한 테스트 케이스 추가
    - Product 개수 변경
    - Option 체크 변경

--- react-tdd-app/frontend/src/components/Type.test.tsx
+++ react-tdd-app/frontend/src/components/Type.test.tsx
@@ -1,11 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Type from './Type';
 import { server } from 'mocks/server';
 import { rest } from 'msw';
+import userEvent from '@testing-library/user-event';
+import { OrderContextProvider } from 'contexts/OrderContext';
 
 describe('Type component', () => {
   test('displays product images from server', async () => {
-    render(<Type orderType="products" />);
+    render(<Type orderType="products" />, { wrapper: OrderContextProvider });
 
     const productImages = (await screen.findAllByRole('img', {
       name: /product/i,
@@ -19,7 +21,7 @@ describe('Type component', () => {
   test('error on fetching product data', async () => {
     server.resetHandlers(rest.get('http://localhost:4000/products', (req, res, ctx) => res(ctx.status(500))));
 
-    render(<Type orderType="products" />);
+    render(<Type orderType="products" />, { wrapper: OrderContextProvider });
 
     const errorBanner = await screen.findByTestId('error-banner');
     expect(errorBanner).toHaveTextContent('에러가 발생했습니다.');
@@ -31,5 +33,30 @@ describe('Type component', () => {
     const optionCheckboxes = await screen.findAllByRole('checkbox');
 
     expect(optionCheckboxes).toHaveLength(2);
+  });
+
+  test("update product's total price when products changed", async () => {
+    render(<Type orderType="products" />, { wrapper: OrderContextProvider });
+
+    const totalProductsPrice = screen.getByText('총 가격', { exact: false });
+    expect(totalProductsPrice).toHaveTextContent('0');
+
+    const americaInput = await screen.findByRole('spinbutton', { name: `America quantity` });
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, '1');
+
+    expect(totalProductsPrice).toHaveTextContent('₩1000');
+  });
+
+  test("update option's total price when options changed", async () => {
+    render(<Type orderType="options" />, { wrapper: OrderContextProvider });
+
+    const totalOptionsPrice = screen.getByText('총 가격', { exact: false });
+    expect(totalOptionsPrice).toHaveTextContent('0');
+
+    const dinnerCheckbox = await screen.findByRole('checkbox', { name: `Dinner option` });
+    userEvent.click(dinnerCheckbox);
+
+    expect(totalOptionsPrice).toBeEnabled();
   });
 });
