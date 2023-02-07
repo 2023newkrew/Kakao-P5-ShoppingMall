4b54880878a0f64394f9e87db266fab0c7638b55
Kim Seung Hwan <dev.appmin@gmail.com>
2023-02-07 17:43:42 +0900

    feat: Implement options feature @ basket store

--- react-tdd-app/frontend/src/stores/use-basket-store.ts
+++ react-tdd-app/frontend/src/stores/use-basket-store.ts
@@ -1,24 +1,43 @@
 import { create } from 'zustand';
 
 type BasketStoreType = {
-  baskets: { [key: string]: number };
-  totalBasketCount: number;
-  setBasket: (productName: string, productCount: number) => void;
+  productsBasket: { [key: string]: number };
+  totalProductsBasketCount: number;
+  setProductsBasket: (productName: string, productCount: number) => void;
+
+  optionsBasket: { [key: string]: boolean };
+  totalOptionsBasketCount: number;
+  setOptionsBasket: (optionName: string, isChecked: boolean) => void;
 };
 
 const useBasketStore = create<BasketStoreType>((set) => ({
-  baskets: {},
-  totalBasketCount: 0,
-  setBasket: (productName, productCount) => {
+  productsBasket: {},
+  totalProductsBasketCount: 0,
+  setProductsBasket: (productName, productCount) => {
     set((state) => ({
-      baskets: {
-        ...state.baskets,
+      productsBasket: {
+        ...state.productsBasket,
         [productName]: productCount,
       },
-      totalBasketCount: Object.values({
-        ...state.baskets,
+      totalProductsBasketCount: Object.values({
+        ...state.productsBasket,
         [productName]: productCount,
       }).reduce((acc, cur) => acc + cur, 0),
+    }));
+  },
+
+  optionsBasket: {},
+  totalOptionsBasketCount: 0,
+  setOptionsBasket: (optionName, isChecked) => {
+    set((state) => ({
+      optionsBasket: {
+        ...state.optionsBasket,
+        [optionName]: isChecked,
+      },
+      totalOptionsBasketCount: Object.values({
+        ...state.optionsBasket,
+        [optionName]: isChecked,
+      }).filter(Boolean).length,
     }));
   },
 }));
