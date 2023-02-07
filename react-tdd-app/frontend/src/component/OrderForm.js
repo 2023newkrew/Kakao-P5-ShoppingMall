9f96d8aba6ec2343f547f38fb1e4e75a98e2e23f
gray-sin <gray.sin@kakaocorp.com>
2023-02-07 18:32:54 +0900

    OrderForm을 OrderFormPage로부터 추출

---
+++ react-tdd-app/frontend/src/component/OrderForm.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import "./OrderForm.css";
+
+const PRODUCT_PRICE = 1000;
+const OPTION_PRICE = 500;
+
+function OrderForm({ productResource, optionResource }) {
+  const products = productResource.read();
+  const options = optionResource.read();
+
+  const [selectedProducts, selectProducts] = useState(() =>
+    products.map(({ name }) => ({
+      name,
+      amount: 0,
+    }))
+  );
+
+  const [selectedOptions, selectOptions] = useState(() =>
+    options.map(({ name }) => ({
+      name,
+      amount: 0,
+    }))
+  );
+
+  const productTotalPrice =
+    PRODUCT_PRICE *
+    selectedProducts.map(({ amount }) => amount).reduce((p, c) => p + c, 0);
+
+  const optionTotalPrice =
+    OPTION_PRICE *
+    selectedOptions.map(({ amount }) => amount).reduce((p, c) => p + c, 0);
+
+  const totalPrice = productTotalPrice + optionTotalPrice;
+
+  const handleProductInputChange = (event) => {
+    const { target } = event;
+
+    const nextSelectedProducts = [...selectedProducts];
+    const targetProduct = nextSelectedProducts.find(
+      ({ name }) => name === target.name
+    );
+    targetProduct.amount = Number(target.value);
+
+    selectProducts(nextSelectedProducts);
+  };
+
+  const handleOptionInputChange = (event) => {
+    const { target } = event;
+
+    const nextSelectedOptions = [...selectedOptions];
+    const targetOption = nextSelectedOptions.find(
+      ({ name }) => name === target.name
+    );
+    targetOption.amount = Number(target.checked);
+
+    selectOptions(nextSelectedOptions);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="order-form" onSubmit={handleSubmit}>
+      <fieldset className="order-form__product-fieldset">
+        <legend>Products</legend>
+        <dl>
+          <dt>하나의 가격</dt>
+          <dd>{PRODUCT_PRICE}</dd>
+          <dt>선택 상품 가격</dt>
+          <dd aria-label="선택 상품 가격">{productTotalPrice}</dd>
+        </dl>
+        <div>
+          {products.map(({ name, imagePath }) => (
+            <label key={name}>
+              <img src={imagePath} alt={`${name} scene`} />
+              {name}
+              <input
+                type="number"
+                name={name}
+                defaultValue="0"
+                onChange={handleProductInputChange}
+              />
+            </label>
+          ))}
+        </div>
+      </fieldset>
+
+      <fieldset className="order-form__option-fieldset">
+        <legend>Options</legend>
+        <dl>
+          <dt>하나의 가격</dt>
+          <dd>{OPTION_PRICE}</dd>
+          <dt>선택 옵션 가격</dt>
+          <dd aria-label="선택 옵션 가격">{optionTotalPrice}</dd>
+        </dl>
+        <div>
+          {options.map(({ name }) => (
+            <label key={name}>
+              <input
+                type="checkbox"
+                name={name}
+                defaultChecked={false}
+                onChange={handleOptionInputChange}
+              />
+              {name}
+            </label>
+          ))}
+        </div>
+      </fieldset>
+
+      <fieldset className="order-form__summary-fieldset">
+        <dl>
+          <dt>Total Price</dt>
+          <dd aria-label="Total Price">{totalPrice}</dd>
+        </dl>
+        <input type="submit" value="주문하기" />
+      </fieldset>
+    </form>
+  );
+}
+
+export default OrderForm;
---
+++ react-tdd-app/frontend/src/component/OrderForm.test.js
@@ -0,0 +1,61 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import OrderForm from "./OrderForm";
+
+const productResource = {
+  read: () => [
+    {
+      name: "America",
+      imagePath: "/images/america.jpeg",
+      description: "Good America",
+    },
+    {
+      name: "England",
+      imagePath: "/images/england.jpeg",
+      description: "Good England",
+    },
+  ],
+};
+
+const optionResource = {
+  read: () => [
+    {
+      name: "Insurance",
+      description: "안전한 여행을 위해서!",
+    },
+    {
+      name: "Dinner",
+      description: "맛있는 저녁과 함께하는 여행!",
+    },
+  ],
+};
+
+test("상품 개수를 변경하면 선택 상품 가격이 변경된다.", () => {
+  render(
+    <OrderForm
+      productResource={productResource}
+      optionResource={optionResource}
+    />
+  );
+
+  const americaInput = screen.getByLabelText("America");
+  const productTotalPriceElement = screen.getByRole("definition", { name: "선택 상품 가격" });
+
+  userEvent.type(americaInput, "3");
+  expect(productTotalPriceElement).toHaveTextContent("3000");
+});
+
+test("옵션을 선택하면 가격이 변경된다.", () => {
+  render(
+    <OrderForm
+      productResource={productResource}
+      optionResource={optionResource}
+    />
+  );
+
+  const insuranceInput = screen.getByRole("checkbox", { name: "Insurance" });
+  const optionTotalPriceElement = screen.getByRole("definition", { name: "선택 옵션 가격" });
+
+  userEvent.click(insuranceInput);
+  expect(optionTotalPriceElement).toHaveTextContent("500");
+});
