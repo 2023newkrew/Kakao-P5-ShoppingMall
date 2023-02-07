9fd8860b3b69a1deea39fa43b09e1cedfb4b3cd1
gray-sin <gray.sin@kakaocorp.com>
2023-02-07 14:17:29 +0900

    test def: OrderFormPage renders product images and option names

--- react-tdd-app/frontend/src/component/OrderFormPage.test.js
+++ react-tdd-app/frontend/src/component/OrderFormPage.test.js
@@ -4,22 +4,37 @@ import { setupServer } from "msw/node";
 import OrderFormPage from "./OrderFormPage";
 
 const server = setupServer(
-  rest.get("/products", (request, response, context) => {
-    return response(
-      context.json([
-        {
-          name: "America",
-          imagePath: "/images/america.jpeg",
-          description: "Good America",
-        },
-        {
-          name: "England",
-          imagePath: "/images/england.jpeg",
-          description: "Good England",
-        },
-      ])
-    );
-  })
+  rest
+    .get("/products", (request, response, context) => {
+      return response(
+        context.json([
+          {
+            name: "America",
+            imagePath: "/images/america.jpeg",
+            description: "Good America",
+          },
+          {
+            name: "England",
+            imagePath: "/images/england.jpeg",
+            description: "Good England",
+          },
+        ])
+      );
+    })
+    .get("/options", (request, response, context) => {
+      return response(
+        context.json([
+          {
+            name: "Insurance",
+            description: "안전한 여행을 위해서!",
+          },
+          {
+            name: "Dinner",
+            description: "맛있는 저녁과 함께하는 여행!",
+          },
+        ])
+      );
+    })
 );
 
 beforeAll(() => server.listen());
@@ -38,7 +53,7 @@ test("renders loading", () => {
   expect(loadingElement).toBeInTheDocument();
 });
 
-test("renders products", async () => {
+test("renders product names", async () => {
   render(<OrderFormPage />);
   const americaElement = await screen.findByText("America");
   expect(americaElement).toBeInTheDocument();
@@ -56,4 +71,24 @@ test("handles server error", async () => {
   render(<OrderFormPage />);
   const errorElement = await screen.findByText("문제가 발생했습니다.");
   expect(errorElement).toBeInTheDocument();
+});
+
+test("renders product images", async () => {
+  render(<OrderFormPage />);
+  const americaImageElement = await screen.findByAltText(
+    "America product image"
+  );
+  expect(americaImageElement).toBeInTheDocument();
+  const englandImageElement = await screen.findByAltText(
+    "England product image"
+  );
+  expect(englandImageElement).toBeInTheDocument();
+});
+
+test("renders option names", async () => {
+  render(<OrderFormPage />);
+  const insuranceElement = await screen.findByText("Insurance");
+  expect(insuranceElement).toBeInTheDocument();
+  const dinnerElement = await screen.findByText("Dinner");
+  expect(dinnerElement).toBeInTheDocument();
 });
