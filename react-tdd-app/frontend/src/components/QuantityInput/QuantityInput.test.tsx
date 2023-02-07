7f2a733d29cec7da176d08f0071b6619ed246008
pyi7628 <pyi7628@gmail.com>
2023-02-07 23:23:01 +0900

    feat: QuantityInput img, 음수 테스트 추가

--- react-tdd-app/frontend/src/components/QuantityInput/QuantityInput.test.tsx
+++ react-tdd-app/frontend/src/components/QuantityInput/QuantityInput.test.tsx
@@ -11,24 +11,35 @@ describe('<QuantityInput />', () => {
 
   it('name text와 input이 잘 생성되어야 한다.', () => {
     const { getByText, getByRole } = render(<QuantityInput {...quantityInputProps} />);
-    getByText('test name');
-    getByRole('spinbutton');
+    getByText(quantityInputProps.name);
+    getByRole('textbox');
   });
 
-  it('input의 value를 숫자로 변경할 때 입력한 값으로 변해야한다.', () => {
+  it('input의 value를 자연수로 변경할 때 입력한 값으로 변해야한다.', () => {
     const { getByRole } = render(<QuantityInput {...quantityInputProps} />);
-    const input = getByRole('spinbutton');
+    const input = getByRole('textbox');
     fireEvent.change(input, {
       target: {
         value: 1,
       },
     });
-    expect(input).toHaveAttribute('value', 1);
+    expect(input).toHaveAttribute('value', '1');
+  });
+
+  it('input의 value를 음수로 변경할 때 입력이 안되고 value는 0이 되어야한다.', () => {
+    const { getByRole } = render(<QuantityInput {...quantityInputProps} />);
+    const input = getByRole('textbox');
+    fireEvent.change(input, {
+      target: {
+        value: -1,
+      },
+    });
+    expect(input).toHaveAttribute('value', '1');
   });
 
   it('input의 value를 숫자외의 문자로 변경할 때 입력이 안되고 value는 null이 되어야한다.', () => {
     const { getByRole } = render(<QuantityInput {...quantityInputProps} />);
-    const input = getByRole('spinbutton');
+    const input = getByRole('textbox');
     fireEvent.change(input, {
       target: {
         value: '문자',
@@ -37,5 +48,17 @@ describe('<QuantityInput />', () => {
     expect(input).toHaveAttribute('value', '');
   });
 
-  // 이미지 잘 들어가나 test
+  it('img의 alt에 상품 이름이 포함 되어야한다.', () => {
+    const { getByRole } = render(<QuantityInput {...quantityInputProps} />);
+    const image = getByRole('img') as HTMLImageElement;
+
+    expect(image.alt).toContain(quantityInputProps.name);
+  });
+
+  it('img의 src에 .jpeg 확장자를 가진 파일 경로가 들어와야 한다. ', () => {
+    const { getByRole } = render(<QuantityInput {...quantityInputProps} />);
+    const image = getByRole('img') as HTMLImageElement;
+
+    expect(image.src).toContain('.jpeg');
+  });
 });
