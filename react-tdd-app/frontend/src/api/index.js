1dc7f87ea8b7c9323cd3bbdf3e6d7fec7986058c
gray-sin <gray.sin@kakaocorp.com>
2023-02-07 15:02:43 +0900

    fetchProducts 수정

--- react-tdd-app/frontend/src/api/index.js
+++ react-tdd-app/frontend/src/api/index.js
@@ -1,7 +1,14 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 function fetchProducts() {
-  return fetch(`${baseUrl}/products`).then((response) => response.json());
+  return fetch(`${baseUrl}/products`)
+    .then((response) => response.json())
+    .then((products) =>
+      products.map((product) => ({
+        ...product,
+        imagePath: `${baseUrl}/${product.imagePath}`,
+      }))
+    );
 }
 
 function fetchOptions() {
