79de566fcfa4afd3d1e1b3d4d873d9db7ee1de38
은승균 <dmstmdrbs98@gmail.com>
2023-02-08 11:54:10 +0900

    chore: CompletePage 생성

---
+++ react-tdd-app/frontend/src/pages/Complete/CompletePage.test.tsx
@@ -0,0 +1,8 @@
+import { render } from 'test-utils';
+import CompletePage from './CompletePage';
+
+describe('complete page', () => {
+  test('should be rendered', () => {
+    render(<CompletePage />);
+  });
+});
---
+++ react-tdd-app/frontend/src/pages/Complete/CompletePage.tsx
@@ -0,0 +1,4 @@
+const CompletePage = () => {
+  return <div>Complete page</div>;
+};
+export default CompletePage;
