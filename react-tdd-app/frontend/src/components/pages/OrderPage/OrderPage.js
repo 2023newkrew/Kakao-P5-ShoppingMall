b33a68f5b7099b87af312345423bd0ba205c0a8d
hape.k <hape.k@kakaocorp.com>
2023-02-08 15:53:24 +0900

    [수정] OrderPage 총 가격 표시 추가

--- react-tdd-app/frontend/src/components/pages/OrderPage/OrderPage.js
+++ react-tdd-app/frontend/src/components/pages/OrderPage/OrderPage.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { OrderContext } from "../../../contexts/OrderContext";
 import OrderContainer from "./OrderContainer/OrderContainer";
 import "./OrderPage.css";
 
 export default function OrderPage() {
+  const { totals } = useContext(OrderContext);
   return (
     <div className="order-page">
       <div className="order-page__top">
@@ -14,7 +16,7 @@ export default function OrderPage() {
           <OrderContainer requestPath="options" />
         </div>
         <div className="order-page__total-price">
-          <h2>Total Price : </h2>
+          <h2>Total Price : {totals.total}</h2>
           <br />
           <button>주문</button>
         </div>
