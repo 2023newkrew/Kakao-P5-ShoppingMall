43dc56ac43fef6e25d9abf4fb764bfa8c972e4f8
은승균 <dmstmdrbs98@gmail.com>
2023-02-08 11:50:27 +0900

    test: test-util 추가
    - react-router mocking
    - custom render 구현

--- react-tdd-app/frontend/src/components/Type.test.tsx
+++ react-tdd-app/frontend/src/components/Type.test.tsx
@@ -1,15 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from 'test-utils';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
 import Type from './Type';
 import { server } from 'mocks/server';
-import { rest } from 'msw';
-import userEvent from '@testing-library/user-event';
-import { OrderContextProvider } from 'contexts/OrderContext';
 import { PRODUCT_PRICE } from 'constants/price';
 import { options } from 'mocks/options';
 
 describe('Type component', () => {
   test('displays product images from server', async () => {
-    render(<Type orderType="products" />, { wrapper: OrderContextProvider });
+    render(<Type orderType="products" />);
 
     const productImages = (await screen.findAllByRole('img', {
       name: /product/i,
@@ -23,7 +22,7 @@ describe('Type component', () => {
   test('error on fetching product data', async () => {
     server.resetHandlers(rest.get('http://localhost:4000/products', (req, res, ctx) => res(ctx.status(500))));
 
-    render(<Type orderType="products" />, { wrapper: OrderContextProvider });
+    render(<Type orderType="products" />);
 
     const errorBanner = await screen.findByTestId('error-banner');
     expect(errorBanner).toHaveTextContent('에러가 발생했습니다.');
@@ -38,7 +37,7 @@ describe('Type component', () => {
   });
 
   test("update product's total price when products changed", async () => {
-    render(<Type orderType="products" />, { wrapper: OrderContextProvider });
+    render(<Type orderType="products" />);
 
     const totalProductsPrice = screen.getByText('총 가격', { exact: false });
     expect(totalProductsPrice).toHaveTextContent('0');
@@ -51,7 +50,7 @@ describe('Type component', () => {
   });
 
   test("update option's total price when options changed", async () => {
-    render(<Type orderType="options" />, { wrapper: OrderContextProvider });
+    render(<Type orderType="options" />);
 
     const totalOptionsPrice = screen.getByText('총 가격', { exact: false });
     expect(totalOptionsPrice).toHaveTextContent('0');
---
+++ react-tdd-app/frontend/src/test-utils.tsx
@@ -0,0 +1,30 @@
+import { ReactElement, ReactNode } from 'react';
+import { RenderOptions, render } from '@testing-library/react';
+import { OrderContextProvider } from 'contexts/OrderContext';
+import { BrowserRouter } from 'react-router-dom';
+const mockedUsedNavigate = jest.fn();
+
+// Mocking react-router hooks
+jest.mock('react-router-dom', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+const AppProviders = ({ children }: { children: ReactNode }) => {
+  return (
+    <OrderContextProvider>
+      <BrowserRouter>{children}</BrowserRouter>
+    </OrderContextProvider>
+  );
+};
+const customRender = (Element: ReactElement, options?: RenderOptions) => {
+  return render(Element, {
+    wrapper: AppProviders,
+    ...options,
+  });
+};
+
+export * from '@testing-library/react';
+// override render method
+export { customRender as render };
