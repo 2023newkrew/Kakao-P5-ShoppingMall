abae1cd32ed9f92d2a8bdf663f6af975d079f460
은승균 <dmstmdrbs98@gmail.com>
2023-02-08 10:25:27 +0900

    chore: 요소 기본 스타일 적용
    - 버튼 스타일
    - heading 스타일

--- react-tdd-app/frontend/src/index.tsx
+++ react-tdd-app/frontend/src/index.tsx
@@ -16,7 +16,23 @@ const GlobalStyle = createGlobalStyle`
   input {
    ${tw`pl-2 border border-gray-500 rounded focus:outline-blue-500`}
   }
+  button {
+    ${tw`bg-blue-600 rounded  p-2 text-white hover:bg-blue-700`}
+  }
+  h1{
+    ${tw`font-bold text-3xl`}
+  }
+  h2{
+    ${tw`font-bold text-2xl`}
+  }
+  h3{
+    ${tw`font-semibold text-xl`}
+  }
+  h4{
+    ${tw`font-semibold text-lg`}
+  }
 `;
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
