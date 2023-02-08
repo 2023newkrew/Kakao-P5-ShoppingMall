18c26c9424bcbf9d44f7e40f4cc428c021f45381
gray-sin <gray.sin@kakaocorp.com>
2023-02-08 20:32:06 +0900

    test def: OrderConfirmPage

---
+++ react-tdd-app/frontend/src/component/OrderConfirmPage.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import OrderConfirmPage from "./OrderConfirmPage";
+
+test("initial checkbox.checked should be false", () => {
+  render(<OrderConfirmPage />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: "주문하려는 것을 확인하셨나요?",
+  });
+  const confirmButton = screen.getByRole("button", { name: "주문 확인" });
+
+  expect(checkbox.checked).toEqual(false);
+  expect(confirmButton.disabled).toBeTruthy();
+});
+
+test("button should be activated by clicking the checkbox", () => {
+  render(<OrderConfirmPage />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: "주문하려는 것을 확인하셨나요?",
+  });
+  const confirmButton = screen.getByRole("button", { name: "주문 확인" });
+
+  userEvent.click(checkbox);
+
+  expect(checkbox.checked).toEqual(true);
+  expect(confirmButton.disabled).toBeFalsy();
+});
