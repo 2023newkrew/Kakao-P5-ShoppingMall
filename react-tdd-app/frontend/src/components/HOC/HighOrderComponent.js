649086d86ba36100e0992bb0e61a470401df862c
ksnd0297 <ksnd0297@gmail.com>
2023-02-08 21:12:17 +0900

    [rename] : 파일 이름 수정

---
+++ react-tdd-app/frontend/src/components/HOC/HighOrderComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../header/Header";
+
+const HighOrderComponent = (InComponent) => {
+  return <InComponent />;
+};
+
+export default HighOrderComponent;
