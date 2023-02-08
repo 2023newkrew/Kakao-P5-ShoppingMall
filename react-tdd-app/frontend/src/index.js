29da289e29754f4a465f3280f64b8cc3ccbe6531
hash.table <hash.table@kakaocorp.com>
2023-02-08 18:51:11 +0900

    feat: browser mock service worker 추가

--- react-tdd-app/frontend/src/index.js
+++ react-tdd-app/frontend/src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { Router } from './Router';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  
+  worker.start();
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
