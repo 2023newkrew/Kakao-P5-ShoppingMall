370797d5c5774d97d2d8006b4cdc06b3ac786337
gray-sin <gray.sin@kakaocorp.com>
2023-02-09 17:16:26 +0900

    ProductListEmptyErrorBoundary 생성

--- react-tdd-app/frontend/src/component/OrderConfirmPage.js
+++ react-tdd-app/frontend/src/component/OrderConfirmPage.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useOrder } from "../context/order";
-import ErrorBoundary from "./ErrorBoundary";
-import OrderConfirm from "./OrderConfirm";
+import OrderConfirm, { ProductListEmptyError } from "./OrderConfirm";
+
+class ProductListEmptyErrorBoundary extends React.Component {
+  state = { error: null };
+  static getDerivedStateFromError(error) {
+    return {
+      error,
+    };
+  }
+  render() {
+    if (this.state.error) {
+      if (this.state.error instanceof ProductListEmptyError) {
+        return this.props.fallback;
+      }
+      throw this.state.error;
+    }
+    return this.props.children;
+  }
+}
 
 function OrderConfirmPage() {
   const { products, options } = useOrder();
@@ -11,13 +28,15 @@ function OrderConfirmPage() {
 
   return (
     <div className="order-confirm-page">
-      <ErrorBoundary fallback={<Navigate to="/" replace={true} />}>
+      <ProductListEmptyErrorBoundary
+        fallback={<Navigate to="/" replace={true} />}
+      >
         <OrderConfirm
           products={products}
           options={options}
           onSubmit={handleSubmit}
         />
-      </ErrorBoundary>
+      </ProductListEmptyErrorBoundary>
     </div>
   );
 }
