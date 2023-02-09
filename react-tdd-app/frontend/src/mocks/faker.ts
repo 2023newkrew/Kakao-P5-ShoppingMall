41fe562db0b5c1793ff50fd4263b672f5fb0f9d0
Kim Seung Hwan <dev.appmin@gmail.com>
2023-02-09 13:27:16 +0900

    test: Create faker response

---
+++ react-tdd-app/frontend/src/mocks/faker.ts
@@ -0,0 +1,19 @@
+export const FAKER_RESPONSE_PRODUCTS = [
+  {
+    name: 'America',
+    imagePath: '/images/america.jpeg',
+  },
+  {
+    name: 'England',
+    imagePath: '/images/england.jpeg',
+  },
+];
+
+export const FAKER_RESPONSE_OPTIONS = [
+  {
+    name: 'Insurance',
+  },
+  {
+    name: 'Dinner',
+  },
+];
--- react-tdd-app/frontend/src/mocks/handler.ts
+++ react-tdd-app/frontend/src/mocks/handler.ts
@@ -1,36 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import { rest } from 'msw';
+import { FAKER_RESPONSE_OPTIONS, FAKER_RESPONSE_PRODUCTS } from './faker';
 import { API_ROUTE, API_ROUTE_PATH } from '@/constants/api';
 
 export const handlers = [
-  rest.get(
-    `${process.env.REACT_APP_API_URL}${API_ROUTE_PATH[API_ROUTE.PRODUCTS]}`,
-    (req, res, ctx) =>
-      res(
-        ctx.json([
-          {
-            name: 'America',
-            imagePath: '/images/america.jpeg',
-          },
-          {
-            name: 'England',
-            imagePath: '/images/england.jpeg',
-          },
-        ]),
-      ),
+  rest.get(`${process.env.REACT_APP_API_URL}${API_ROUTE_PATH[API_ROUTE.PRODUCTS]}`, (_, res, ctx) =>
+    res(ctx.json(FAKER_RESPONSE_PRODUCTS)),
   ),
-  rest.get(
-    `${process.env.REACT_APP_API_URL}${API_ROUTE_PATH[API_ROUTE.OPTIONS]}`,
-    (req, res, ctx) =>
-      res(
-        ctx.json([
-          {
-            name: 'Insurance',
-          },
-          {
-            name: 'Dinner',
-          },
-        ]),
-      ),
+  rest.get(`${process.env.REACT_APP_API_URL}${API_ROUTE_PATH[API_ROUTE.OPTIONS]}`, (_, res, ctx) =>
+    res(ctx.json(FAKER_RESPONSE_OPTIONS)),
   ),
 ];
