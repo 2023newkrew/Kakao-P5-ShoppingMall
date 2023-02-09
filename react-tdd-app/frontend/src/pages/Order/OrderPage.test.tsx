40399e22eb6d15378b75cd6e36bde4b890d0cfd7
은승균 <dmstmdrbs98@gmail.com>
2023-02-10 00:59:33 +0900

    test: OrderPage 옵션 선택  테스트케이스 추가
    - option 선택, 상품 선택 x 시에 라우팅 버튼 비활성화

--- react-tdd-app/frontend/src/pages/Order/OrderPage.test.tsx
+++ react-tdd-app/frontend/src/pages/Order/OrderPage.test.tsx
@@ -1,12 +1,31 @@
-import { render, screen, act } from 'test-utils';
+import { render, screen, act, waitFor } from 'test-utils';
 import OrderPage from './OrderPage';
+import userEvent from '@testing-library/user-event';
 
 describe('OrderPage test', () => {
-  test('Disable options when products are not selected', async () => {
+  test('Disable routing button when products and options are not selected', async () => {
     render(<OrderPage />);
     await act(async () => {
       const nextButton = await screen.findByRole('button', { name: '주문' });
       expect(nextButton).toBeDisabled();
     });
+  });
+
+  test('Disable routing button when products are not selected', async () => {
+    render(<OrderPage />);
+
+    const { totalOptionsPrice, nextButton, dinnerCheckbox } = await waitFor(async () => {
+      const [, totalOptionsPrice] = await screen.findAllByText('총 가격', { exact: false });
+      const nextButton = await screen.findByRole('button', { name: '주문' });
+      const dinnerCheckbox = await screen.findByRole('checkbox', { name: `Dinner option` });
+
+      return { totalOptionsPrice, nextButton, dinnerCheckbox };
+    });
+
+    expect(totalOptionsPrice).toHaveTextContent('0');
+    userEvent.click(dinnerCheckbox);
+    expect(totalOptionsPrice).toHaveTextContent('500');
+
+    expect(nextButton).toBeDisabled();
   });
 });
