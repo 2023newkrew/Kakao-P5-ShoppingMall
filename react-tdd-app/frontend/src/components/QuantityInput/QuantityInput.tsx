4c95296ee18dfa9abfe85d9e9733b34d3e705768
pyi7628 <pyi7628@gmail.com>
2023-02-09 10:57:08 +0900

    feat: product order page 테스트 추가

--- react-tdd-app/frontend/src/components/QuantityInput/QuantityInput.tsx
+++ react-tdd-app/frontend/src/components/QuantityInput/QuantityInput.tsx
@@ -1,16 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { OrderStateContext } from 'contexts/OrderContext';
 import { QuantityInputProps } from 'types';
+import { removeCharacter } from 'utils/shared';
 
 function QuantityInput({ name, imagePath, description, updateOrder }: QuantityInputProps) {
   const [quantity, setQuantity] = useState(0 as number);
   const { order } = useContext(OrderStateContext);
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const regex = /[^0-9]/g;
     const { value } = event.target;
-    setQuantity(Number(value.replace(regex, '')));
-    updateOrder(Number(value.replace(regex, '')), name);
+    setQuantity(removeCharacter(value));
+    updateOrder(removeCharacter(value), name);
   };
 
   useEffect(() => {
--- react-tdd-app/frontend/src/pages/ProductOrderPage/ProductOrderPage.test.tsx
+++ react-tdd-app/frontend/src/pages/ProductOrderPage/ProductOrderPage.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
 import { server } from 'mocks/server';
 import { ProductOrderPage } from 'pages';
+import { removeCharacter } from 'utils/shared';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -22,5 +23,45 @@ describe('<ProductOrderPage />', () => {
 
   test('[주문하기] 버튼을 눌렀을 때 주문 확인 페이지로 넘어가야 한다.', () => {});
 
-  test('travel product를 선택하지 않은 경우에는 [주문하기] 버튼이 disable 되어야 한다.', () => {});
+  test('travel product를 선택하지 않은 경우에는 [주문하기] 버튼이 disable 되어야 한다.', async () => {
+    const { findAllByRole, findByRole } = render(<ProductOrderPage />);
+    const travelInputs = await findAllByRole('textbox');
+    const optionInputs = await findAllByRole('checkbox');
+    const orderButton = (await findByRole('button', { name: '주문하기' })) as HTMLButtonElement;
+
+    fireEvent.change(travelInputs[0], {
+      target: {
+        value: 3,
+      },
+    });
+    fireEvent.change(travelInputs[0], {
+      target: {
+        value: 0,
+      },
+    });
+    fireEvent.click(optionInputs[0]);
+    expect(orderButton.disabled).toBe(true);
+  });
+
+  test('travel product와 option product를 선택했을 때, Total Price는 각 product의 총 가격을 합친 값이어야한다.', async () => {
+    const { findAllByText, findByRole, findAllByRole } = render(<ProductOrderPage />);
+    const productTotalPrices = (await findAllByText(/총합:/)) as HTMLParagraphElement[];
+    const totalPrice = (await findByRole('heading', { name: /Total Price:/ })) as HTMLHeadingElement;
+    const travelInputs = (await findAllByRole('textbox')) as HTMLInputElement[];
+    const optionInputs = (await findAllByRole('checkbox')) as HTMLInputElement[];
+
+    fireEvent.change(travelInputs[0], {
+      target: {
+        value: 3,
+      },
+    });
+    fireEvent.click(optionInputs[0]);
+
+    let totalSum = 0;
+    for (const productTotalPrice of productTotalPrices) {
+      totalSum += removeCharacter(productTotalPrice.innerHTML);
+    }
+
+    expect(totalPrice.innerHTML).toBe(`Total Price: $${totalSum}`);
+  });
 });
