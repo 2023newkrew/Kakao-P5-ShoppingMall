3380127cf80aadecbc120b9748226d50c55e1658
gray-sin <gray.sin@kakaocorp.com>
2023-02-09 17:29:24 +0900

    ErrorBoundary에 대한 테스트 코드 추가

--- react-tdd-app/frontend/src/component/ErrorBoundary.test.js
+++ react-tdd-app/frontend/src/component/ErrorBoundary.test.js
@@ -26,3 +26,59 @@ test("renders fallback", () => {
   const helloElement = screen.getByText("hello2");
   expect(helloElement).toBeInTheDocument();
 });
+
+test("should catch by errorType prop", () => {
+  class SomeSpecialError extends Error {
+    constructor() {
+      super("some special error");
+      this.name = this.constructor.name;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  function Thrower() {
+    throw new SomeSpecialError();
+  }
+
+  render(
+    <ErrorBoundary errorType={SomeSpecialError} fallback={<div>hello2</div>}>
+      <Thrower />
+    </ErrorBoundary>
+  );
+
+  const helloElement = screen.getByText("hello2");
+  expect(helloElement).toBeInTheDocument();
+});
+
+test("should catch by errorType prop - 2", () => {
+  class SomeSpecialError extends Error {
+    constructor() {
+      super("some special error");
+      this.name = this.constructor.name;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  class OtherError extends Error {
+    constructor() {
+      super("other error");
+      this.name = this.constructor.name;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  function Thrower() {
+    throw new SomeSpecialError();
+  }
+
+  render(
+    <ErrorBoundary fallback={<div>omg!</div>}>
+      <ErrorBoundary errorType={OtherError} fallback={<div>hello2</div>}>
+        <Thrower />
+      </ErrorBoundary>
+    </ErrorBoundary>
+  );
+
+  const helloElement = screen.getByText("omg!");
+  expect(helloElement).toBeInTheDocument();
+});
