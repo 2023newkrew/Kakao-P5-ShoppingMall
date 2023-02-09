f24c9e71a68e36a0561a8ae56fccef74c02daf43
kuma.koh <kuma.koh@kakaocorp.com>
2023-02-09 15:06:33 +0900

    feat: 라우팅 설정

--- react-tdd-app/frontend/src/App.jsx
+++ react-tdd-app/frontend/src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Order from './pages/order';
 import Confirm from './pages/confirm';
 
 export default function App() {
   return (
-    <>
-      <Order />
-      <Confirm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Order />} />
+        <Route path="/confirm" element={<Confirm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
