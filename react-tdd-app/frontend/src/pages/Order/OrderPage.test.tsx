bb4af45be410c22e526fd213a37ecd72c335102b
은승균 <dmstmdrbs98@gmail.com>
2023-02-10 11:19:04 +0900

    test: OrderPage 전체 시나리오 테스트케이스
    - 주문 상품, 옵션 선택 과정 테스트

--- react-tdd-app/frontend/src/pages/Order/OrderPage.test.tsx
+++ react-tdd-app/frontend/src/pages/Order/OrderPage.test.tsx
@@ -15,7 +15,7 @@ describe('OrderPage test', () => {
     render(<OrderPage />);
 
     const { totalOptionsPrice, nextButton, dinnerCheckbox } = await waitFor(async () => {
-      const [, totalOptionsPrice] = await screen.findAllByText('총 가격', { exact: false });
+      const totalOptionsPrice = await screen.findByText('옵션 총 가격', { exact: false });
       const nextButton = await screen.findByRole('button', { name: '주문' });
       const dinnerCheckbox = await screen.findByRole('checkbox', { name: `Dinner option` });
 
@@ -33,7 +33,7 @@ describe('OrderPage test', () => {
     render(<OrderPage />);
 
     const { totalProductsPrice, nextButton, americaInput } = await waitFor(async () => {
-      const [totalProductsPrice] = await screen.findAllByText('총 가격', { exact: false });
+      const totalProductsPrice = await screen.findByText('상품 총 가격', { exact: false });
       const americaInput = await screen.findByRole('spinbutton', { name: `America quantity` });
 
       const nextButton = await screen.findByRole('button', { name: '주문' });
@@ -51,5 +51,42 @@ describe('OrderPage test', () => {
     await waitFor(() => {
       expect(nextButton).toBeEnabled();
     });
+  });
+
+  test('update options and products', async () => {
+    render(<OrderPage />);
+
+    const { totalPrice, totalProductsPrice, totalOptionsPrice, insuranceCheckbox, dinnerCheckbox, productInputs } =
+      await waitFor(async () => {
+        const totalPrice = await screen.findByText('총 주문 가격', { exact: false });
+        const totalProductsPrice = await screen.findByText('상품 총 가격', { exact: false });
+        const productInputs = await screen.findAllByRole('spinbutton', { name: /quantity/i });
+        const totalOptionsPrice = await screen.findByText('옵션 총 가격', { exact: false });
+        const insuranceCheckbox = await screen.findByRole('checkbox', { name: 'Insurance option' });
+        const dinnerCheckbox = await screen.findByRole('checkbox', { name: 'Dinner option' });
+
+        return { totalPrice, totalProductsPrice, totalOptionsPrice, insuranceCheckbox, dinnerCheckbox, productInputs };
+      });
+
+    productInputs.forEach((productInput) => {
+      userEvent.clear(productInput);
+    });
+    expect(totalOptionsPrice).toHaveTextContent('0');
+
+    userEvent.click(dinnerCheckbox);
+    expect(totalOptionsPrice).toHaveTextContent('500');
+
+    userEvent.click(insuranceCheckbox);
+    expect(totalOptionsPrice).toHaveTextContent('1000');
+
+    const [americaInput, englandInput] = productInputs;
+
+    userEvent.type(americaInput, '2');
+    expect(totalProductsPrice).toHaveTextContent('2000');
+
+    userEvent.type(englandInput, '4');
+    expect(totalProductsPrice).toHaveTextContent('6000');
+
+    expect(totalPrice).toHaveTextContent('7000');
   });
 });
